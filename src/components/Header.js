43408e2c05274aa9d62b6a0988521d0cb7ab833d
anasfaz <anaspkmmc@gmail.com>
2023-09-29 22:07:31 +0530

    Login Logout butoon add in header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 const Header = () => {
-    return (
-      <div className="header">
-        <div>
-          <img
-            className="logo-img"
-            src={LOGO_URL}
-            alt="logo"
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>home</li>
-            <li>contact</li>
-            <li>About as</li>
-            <li>Cart</li>
-          </ul>
-            </div>
-            {console.log('headers--------------')}
+  const [btnName,setBtnName]=useState('Login')
+  const loginChange = () => {
+    if (btnName == 'Login') {
+      setBtnName('Logout')
+    } else if (btnName == 'Logout') {
+      setBtnName('Login')
+    }
+    console.log(btnName,'log');
+  }
+  return (
+    <div className="header">
+      <div>
+        <img className="logo-img" src={LOGO_URL} alt="logo" />
       </div>
-    );
+      <div className="nav-items">
+        <ul>
+          <li>home</li>
+          <li>contact</li>
+          <li>About as</li>
+          <li>Cart</li>
+          <button className="login-btn" onClick={loginChange}>{btnName}</button>
+        </ul>
+      </div>
+      {console.log("headers--------------")}
+    </div>
+  );
 };
-  
+
 export default Header;
