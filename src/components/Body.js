f2ebbf723aad66730ff8391e798725b731f85035
anasfaz <anaspkmmc@gmail.com>
2023-09-29 21:22:37 +0530

    shimmer component add

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,6 @@
 import RestaurantCard from "./ResturantCard";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
   //Local  state Variable by react - Super powered variable
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
@@ -8,32 +9,10 @@ const Body = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  const postData = {
-    lat: 10.7837491,
-    lng: 76.0076374,
-    nextOffset: "COVCELQ4KICQ3ZDemazzHDCnEzgE",
-    widgetOffset: {
-      collectionV5RestaurantListWidget_SimRestoRelevance_food_seo: "10",
-      inlineFacetFilter: "",
-      restaurantCountWidget: "",
-    },
-    filters: {},
-    seoParams: {
-      seoUrl: "https://www.swiggy.com/",
-      pageType: "FOOD_HOMEPAGE",
-      apiName: "FoodHomePage",
-    },
-    page_type: "DESKTOP_WEB_LISTING",
-    _csrf: "ZhScg6fUfcEA-Oz1a2ekdeeTCU8a8umAZL1k4DG0",
-  };
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(postData),
-  };
+
+
   // mobile width api call url "https://www.swiggy.com/mapi/homepage/getCards?lat=10.7837491&lng=76.0076374"
+  // web width api "https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.7837491&lng=76.0076374&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
   const fetchData = async () => {
     const Data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.7837491&lng=76.0076374&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
@@ -42,15 +21,14 @@ const Body = () => {
 
     const { data } = json;
 
-    console.log(
-      data.cards[4],
-      "json"
-    );
+    console.log(data.cards[3], "json");
     setListOfRestaurants(
-      data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+      data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
-
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer/>
+  }
   return (
     <div className="body">
       <div className="filter">
