e9f2ab1b39bf388a808104d124e0be6d3a46b46c
anasfaz <anaspkmmc@gmail.com>
2023-10-09 10:15:07 +0530

    react router dom add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "../index.css";
 import Header from'./components/Header'
 import Body from './components/Body'
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./components/Error";
 
 
 
@@ -84,6 +88,23 @@ const AppLayout = () => {
     </div>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement:<Error/>
+    
+  },
+  {
+    path: "/about",
+    element: <About/>
+  },
+  {
+    path: '/contact',
+    element:<Contact/>
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+
+const Error = () => {
+    return (
+        <h1>404 Error something wrong</h1>
+    )
+}
+
+export default Error;
