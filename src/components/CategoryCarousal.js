1ee609e43a1e97d365a6916934b39c1674c38fea
anasfaz <anaspkmmc@gmail.com>
2024-01-23 22:36:40 +0530

    category image slide bar ✅

--- src/components/CategoryCarousal.js
+++ src/components/CategoryCarousal.js
@@ -1,42 +1,63 @@
 import React, { useRef } from "react";
 
 function CategoryCarousal() {
-    let box = useRef(null)
-    // const btnPressNext = () => {
-    //     let width = box.clientWidth || box.getBoundingClientRect().width
-    //     let box.scrollLeft=
-    // }
-    function handleScrollToFirstCat() {
-        
-    }
+  const sliderRef = useRef(null);
+  const scrollAmount = 500;
+
   return (
     <>
       <div
-        className="bg-slate-500 w-full flex justify-around 
+        className=" mx-[calc(10%+34px)] bg-slate-500 w-full flex justify-around 
     overflow-hidden relative "
       >
         <h2 className="text-2xl font-bold ">What's on your mind?</h2>
         <div className="flex ">
-          <button>
+          <button
+            onClick={() => {
+              const container = sliderRef.current;
+              container.scrollLeft -= scrollAmount;
+              
+              console.log(container.scrollLeft);
+            }}
+          >
             <p>⬅️</p>
           </button>
-          <button>
+          <button
+            onClick={() => {
+              const container = sliderRef.current;
+              console.log(container, "container");
+              container.scrollLeft += scrollAmount;
+              console.log(container.scrollLeft);
+            }}
+          >
             <p>➡️</p>
           </button>
         </div>
       </div>
-      <div ref={box} className="flex overflow-x-hidden scroll-smooth">
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
-        <div className="h-[180px] w-[144px] bg-slate-500">1</div>
+      <div
+        ref={sliderRef}
+        className="product-container mx-[calc(10%+34px)] flex overflow-x-hidden overflow-y-hidden 
+        overflow-scroll scroll-smooth transition duration-300 ease-in-out"
+      >
+        <div className="h-[180px]  w-[144px] min-w-[144px] min-h-[180px] bg-slate-500">
+          1
+        </div>
+        <div className=" min-w-[144px] min-h-[180px] bg-slate-500">2</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-green-300">3</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-red-500">4</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-orange-500">1</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-blue-500">5</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-white-500">1</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-slate-500">7</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-black-500">8</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-slate-500">9</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-yellow-500">10</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-blue-500">5</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-white-500">1</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-slate-500">7</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-black-500">8</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-slate-500">9</div>
+        <div className=" min-w-[144px] min-h-[180px] bg-yellow-500">10</div>
       </div>
     </>
   );
