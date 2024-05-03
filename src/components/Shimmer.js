69fc87142db5c8801842ba2253e8ff65a1771a89
anasfaz <anaspkmmc@gmail.com>
2024-05-03 19:22:52 +0530

    shimmer animation effect style mb add

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -4,7 +4,7 @@ const Shimmer = () => {
   return (
     <div className="mx-[calc(10%+30px)]  ">
       <div className="grid grid-cols-[repeat(4,1fr)]  gap-5 mx-4 my-8 p-0">
-        <div>
+        <div className="mb-20">
           <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
             <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
             <div class="p-3 ">
@@ -20,7 +20,7 @@ const Shimmer = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="mb-20">
           <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
             <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
             <div class="p-3 ">
@@ -36,7 +36,7 @@ const Shimmer = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="mb-20">
           <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
             <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
             <div class="p-3 ">
@@ -52,7 +52,7 @@ const Shimmer = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="mb-20">
           <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
             <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
             <div class="p-3 ">
@@ -67,12 +67,11 @@ const Shimmer = () => {
               </div>
             </div>
           </div>
-          
-              </div>
-              <div>
-              <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
+        </div>
+        <div className="mb-20">
+          <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
             <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
-            <div class="p-3 ">
+            <div class="p-3 mb-3">
               <div class="grid grid-cols-3 gap-4 mt-2">
                 <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
                 <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
@@ -84,7 +83,55 @@ const Shimmer = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="mb-20">
+          <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
+            <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
+            <div class="p-3 mb-3">
+              <div class="grid grid-cols-3 gap-4 mt-2">
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 col-span-2 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded  animate-pulse"></div>
+                <div class="..."></div>
+                <div class="col-span-2 "></div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="mb-20">
+          <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
+            <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
+            <div class="p-3 mb-3">
+              <div class="grid grid-cols-3 gap-4 mt-2">
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 col-span-2 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded  animate-pulse"></div>
+                <div class="..."></div>
+                <div class="col-span-2 "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mb-20">
+          <div class="mx-auto bg-white  shadow-lg h-[182px] w-[273px] rounded-2xl mb-4">
+            <div class="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse"></div>
+            <div class="p-3 mb-3">
+              <div class="grid grid-cols-3 gap-4 mt-2">
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 col-span-2 bg-gray-200 rounded animate-pulse"></div>
+                <div class="h-8 bg-gray-200 rounded  animate-pulse"></div>
+                <div class="..."></div>
+                <div class="col-span-2 "></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
