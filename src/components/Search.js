eff6a95c5993c132567f79a84d902b8f11564de4
anasfaz <anaspkmmc@gmail.com>
2024-05-05 12:56:35 +0530

    debunce functionality add

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import useDebounce from "../utils/useDebounce";
 
 const Search = () => {
-  return (
-      <div>
-          <input className='border-2 border-gray-500' type="text" />
-    </div>
-  )
-}
+  const [searchList, setSearchList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const debouncedValue = useDebounce(searchQuery, 2000);
+  useEffect(() => {
+    searchFetch();
+  }, [debouncedValue]);
 
-export default Search
+  const searchFetch = async () => {
+    const endpoint = "https://www.swiggy.com/dapi/restaurants/search/suggest";
+    const lat = 9.938136399999992;
+    const lng = 76.3217758;
+
+    // Construct the full URL with parameters
+    const url = `${endpoint}?lat=${lat}&lng=${lng}&str=${debouncedValue}`;
+
+    // Make the fetch request
+    const response = await fetch(url);
+
+    const { data } = await response.json();
+    console.log(data, "responser");
+    setSearchList(data);
+  };
+
+  return (
+    <div className="flex justify-center mt-8">
+      <input
+        onChange={(e) => setSearchQuery(e.target.value)}
+        value={searchQuery}
+        className="border-2 border-gray-500 w-6/12 p-2"
+        type="text"
+      />
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/utils/useDebounce.js
@@ -0,0 +1,22 @@
+import { Input } from "postcss";
+import { useState, useEffect } from "react";
+
+
+const useDebounce = (inputValue, delay) => {
+    const [debounceValue, setDebounceValue] = useState()
+
+    useEffect(() => {
+      
+        const timer = setTimeout(() => {
+         setDebounceValue(inputValue)
+     },delay)
+    
+      return () => {
+        clearTimeout(timer)
+      }
+    }, [inputValue,delay])
+    
+   return debounceValue
+}
+
+export default useDebounce;
